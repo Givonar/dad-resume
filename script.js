7c166da0575b263860bc6b5bc7849b3eab62282b
karkit <141783969+karkit7117@users.noreply.github.com>
2024-05-14 17:03:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById('call-button').addEventListener('click', function() {
     window.location.href = 'tel:+989122680423'; // Replace '099537' with the desired phone number
 });
 document.addEventListener("DOMContentLoaded", function() {
-  const callbutton = document.querySelector(".call-button");
-  callButton.addEventListener("click", function() {
-    window.open('https://www.google.com', '_blank');
+  const mojavezbutton = document.querySelector(".mojavez-button");
+  mojavezButton.addEventListener("click", function() {
+    window.open('https://qr.mojavez.ir/track/3033942', '_blank');
   });
 });
