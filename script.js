5e909eafc6ee6d79f2fe66505e8c82da22e055ef
karkit <141783969+karkit7117@users.noreply.github.com>
2024-05-14 17:02:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('call-button').addEventListener('click', function() {
     window.location.href = 'tel:+989122680423'; // Replace '099537' with the desired phone number
 });
+document.addEventListener("DOMContentLoaded", function() {
+  const callbutton = document.querySelector(".call-button");
+  callButton.addEventListener("click", function() {
+    window.open('https://www.google.com', '_blank');
+  });
+});
