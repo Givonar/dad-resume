c8c672205467a962e1f90675cd9b9f7c740f599f
karkit <141783969+karkit7117@users.noreply.github.com>
2024-05-16 01:15:05 +0330

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('call-button').addEventListener('click', function() {
-    window.location.href = 'tel:+989122680423'; // Replace '099537' with the desired phone number
-});
-document.addEventListener("DOMContentLoaded", function() {
-  const mojavezbutton = document.querySelector(".mojavez-button");
-  mojavezbutton.addEventListener("click", function() {
-    window.open('https://qr.mojavez.ir/track/3033942', '_blank');
-  });
-});
